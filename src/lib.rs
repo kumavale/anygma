7b23fd686bc50b709037976d4c8bc088fb1bb864
kumavale <kumavale.hyperion@gmail.com>
2024-02-03 20:34:20 +0900

    feat: support empty array for `ary_ref`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_export]
 macro_rules! ary_ref {
+    () => { (&[]) as &[&dyn std::any::Any] };
     ( $( $value:expr ),+ $(,)? ) => {
         &[ $(&$value as &dyn std::any::Any),+ ] as &[&dyn std::any::Any]
     };
@@ -11,6 +12,8 @@ mod tests {
 
     #[test]
     fn test_ary_ref() {
+        let a = ary_ref![];
+        assert!(a.is_empty());
         let a = ary_ref![0];
         assert_eq!(a[0].downcast_ref::<i32>(), Some(&0));
     }
